2f31d51e49686d7386299d15dcdda6972677e595
Aluno <Aluno@CCXS314>
2025-06-02 19:37:33 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int largura, comprimento, areaterreno, terrenos;
+
+Console.WriteLine("Digite a largura do terreno (em metros) : ");
+largura = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o comprimento do terreno : ");
+comprimento = Convert.ToInt32(Console.ReadLine());
+
+areaterreno = largura * comprimento;
+
+Console.WriteLine("A aréa do terreno e aréa metros quadrados ");
